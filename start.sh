8fa80b243665984e99cd791360bbb55509bd0914
0XSaurabhSharma <101334796+Git-SaurabhSharma@users.noreply.github.com>
2025-08-17 19:40:43 +0530

    Fix: changed ui port

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
 
 
-streamlit run streamlit_app.py --server.port=8501 --server.address=0.0.0.0
+streamlit run streamlit_app.py --server.port=8080 --server.address=0.0.0.0
