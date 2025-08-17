ee525fe79afe8c4249e40d906b48599345d9b74f
0XSaurabhSharma <101334796+Git-SaurabhSharma@users.noreply.github.com>
2025-08-17 20:56:40 +0530

    fix

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 # start.sh
 uvicorn main:app --host 0.0.0.0 --port 8000 &
-streamlit run frontend.py --server.port=8080  --server.address=0.0.0.0
+streamlit run frontend.py --server.port=$PORT --server.address=0.0.0.0
